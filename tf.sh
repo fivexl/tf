4b6a9808a0be17328adf5974ccd0a42fd38bab5e
Vladimir Samoylov <51059484+cageyv@users.noreply.github.com>
2021-10-18 22:51:14 +0700

    fix TF_CLI_ARGS

--- tf.sh
+++ tf.sh
@@ -115,10 +115,12 @@ ${TF_TERRAFORM_EXECUTABLE} init -backend-config "key=${TF_STATE_PATH}" -backend-
 
 # figure out which env file to use
 if [ -e ./${TF_ENVIRONMENT_ID}.tfvars ]; then
-  export TF_CLI_ARGS_plan="-var-file=./${TF_ENVIRONMENT_ID}.tfvars"
-  export TF_CLI_ARGS_import="-var-file=./${TF_ENVIRONMENT_ID}.tfvars"
   # If we are not applying saving plan, add -var-file
-  [ -z "${TF_AUTO_APPLY_SAVED_PLAN}" ] && export TF_CLI_ARGS_apply="-var-file=./${TF_ENVIRONMENT_ID}.tfvars -auto-approve"
+  if [ -z "${TF_AUTO_APPLY_SAVED_PLAN}" ]; then
+    export TF_CLI_ARGS="-var-file=./${TF_ENVIRONMENT_ID}.tfvars"
+    # Add -auto-approve for any apply commands
+    export TF_CLI_ARGS_apply="-var-file=./${TF_ENVIRONMENT_ID}.tfvars -auto-approve"
+  fi
 fi
 
 ${TF_TERRAFORM_EXECUTABLE} $*
